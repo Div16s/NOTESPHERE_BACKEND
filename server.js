11ca8fcbafa1ef0a8fc45399712a6e50b40903ba
Divyankar Shah <divyankarshah1602@gmail.com>
2023-09-30 15:34:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 const corsOptions = {
   origin: 'https://notesphere-flame.vercel.app', // Replace with your frontend URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
 };
 
 app.use(cors(corsOptions));
